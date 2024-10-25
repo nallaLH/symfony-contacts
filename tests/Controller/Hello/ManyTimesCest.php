4598522d8d61a764b17e894a1df719b4724c015a
Allan Laheu <allan.laheu@etudiant.univ-reims.fr>
2024-10-25 10:34:20 +0200

    Création Cest dédié à l'action manyTimes() du HelloController

---
+++ tests/Controller/Hello/ManyTimesCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Tests\Controller\Hello;
+
+use App\Tests\Support\ControllerTester;
+
+class ManyTimesCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}

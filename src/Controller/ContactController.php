9ecbdaa78f9621bd6bb137514122d03a26e77932
Allan Laheu <allan.laheu@etudiant.univ-reims.fr>
2024-10-25 12:12:26 +0200

    Suppression éléments inutiles de l'action et de la vue + demande du service ContactRepository (push fin de séance)

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ContactRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(ContactRepository $contactRepository): Response
+    {
+        return $this->render('contact/index.html.twig');
+    }
+}

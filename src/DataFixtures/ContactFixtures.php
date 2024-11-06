6be1863473470d3418c7431daa0941987ce8c0f5
Allan Laheu <allan.laheu@etudiant.univ-reims.fr>
2024-11-06 11:14:06 +0100

    suppression corps de la méthode load

---
+++ src/DataFixtures/ContactFixtures.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ContactFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+    }
+}

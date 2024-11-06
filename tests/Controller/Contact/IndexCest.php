42e598cef965754c4eea49a1c6d94c99c4ddcdc3
Allan Laheu <allan.laheu@etudiant.univ-reims.fr>
2024-11-06 10:30:47 +0100

    ajout du test contactLinkTest vérifiant que la route est la bonne après avoir cliqué sur le premier lien

--- tests/Controller/Contact/IndexCest.php
+++ tests/Controller/Contact/IndexCest.php
@@ -16,6 +16,16 @@ class IndexCest
         $I->seeNumberOfElements('a', 195);
     }
 
+    public function contactLinkTest(ControllerTester $I)
+    {
+        $I->amOnPage('/contact');
+        $I->seeResponseCodeIs(200);
+        $I->seeInTitle('Liste des contacts');
+        $I->see('Liste des contacts', 'h1');
+        $I->click('a');
+        $I->seeCurrentRouteIs('app_contact_show');
+    }
+
     //    // tests
     //    public function tryToTest(ControllerTester $I)
     //    {

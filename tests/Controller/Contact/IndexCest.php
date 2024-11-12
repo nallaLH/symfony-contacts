ddd242f802cf49803a041448a2e3be433435d995
Allan <allan.laheu@etudiant.univ-reims.fr>
2024-11-13 00:40:27 +0100

    Modification contactsListTest, contactLinkTest et ajout firstContactClickTest et contactsAreSortedTest test contactLinkTest ne passe pas

--- tests/Controller/Contact/IndexCest.php
+++ tests/Controller/Contact/IndexCest.php
@@ -2,18 +2,21 @@
 
 namespace App\Tests\Controller\Contact;
 
+use App\Factory\ContactFactory;
 use App\Tests\Support\ControllerTester;
+use PHPUnit\Framework\Assert;
 
 class IndexCest
 {
-    public function contactTest(ControllerTester $I)
+    public function contactsListTest(ControllerTester $I)
     {
+        ContactFactory::createMany(5);
         $I->amOnPage('/contact');
         $I->seeResponseCodeIs(200);
         $I->seeInTitle('Liste des contacts');
         $I->see('Liste des contacts', 'h1');
-        $I->seeNumberOfElements('li', 195);
-        $I->seeNumberOfElements('a', 195);
+        $I->seeNumberOfElements('li', 5);
+        $I->seeNumberOfElements('a', 5);
     }
 
     public function contactLinkTest(ControllerTester $I)
@@ -22,10 +25,44 @@ class IndexCest
         $I->seeResponseCodeIs(200);
         $I->seeInTitle('Liste des contacts');
         $I->see('Liste des contacts', 'h1');
-        $I->click('a');
+        $I->click('a.contact-link');
+//        $I->click('a[class*="contact-link"]');
         $I->seeCurrentRouteIs('app_contact_show');
     }
 
+    public function firstContactClickTest(ControllerTester $I)
+    {
+        ContactFactory::createOne(['lastName' => 'Aaaaaaaaaaaaaaa', 'firstName' => 'Joe']);
+        ContactFactory::createMany(5);
+        $I->amOnPage('/contact');
+        $I->click('Aaaaaaaaaaaaaaa, Joe');
+        $I->seeResponseCodeIs(200);
+        $I->seeCurrentRouteIs('app_contact_show');
+    }
+
+    public function contactsAreSortedTest(ControllerTester $I)
+    {
+        ContactFactory::createSequence(
+            [
+                ['lastName' => 'Dupont', 'firstName' => 'Alice'],
+                ['lastName' => 'Martin', 'firstName' => 'Hugo'],
+                ['lastName' => 'Legrand', 'firstName' => 'Thomas'],
+                ['lastName' => 'Dupont', 'firstName' => 'David'],
+                ['lastName' => 'Petit', 'firstName' => 'Bob'],
+            ]
+        );
+
+        $I->amOnPage('/contact');
+        $actualOrder = $I->grabMultiple('a');
+        $expectedOrder = [
+            'Dupont, Alice',
+            'Dupont, David',
+            'Legrand, Thomas',
+            'Martin, Hugo',
+            'Petit, Bob'
+        ];
+        Assert::assertEquals($expectedOrder, $actualOrder);
+    }
     //    // tests
     //    public function tryToTest(ControllerTester $I)
     //    {

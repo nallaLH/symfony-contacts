8f8e8481cff7a490dd9874575e8b1cbdbf0e7e18
Allan Laheu <allan.laheu@etudiant.univ-reims.fr>
2024-11-20 11:09:00 +0100

    modification: méthode index prend désormais une valeur Request et la valeur search est passée à la vue

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,20 +5,28 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(ContactRepository $contactRepository): Response
+    public function index(ContactRepository $contactRepository, Request $request): Response
     {
-        return $this->render('contact/index.html.twig', ['contactRepository' => $contactRepository->findBy([], ['lastname' => 'ASC', 'firstname' => 'ASC'])]);
+        $search = $request->query->get('search', '');
+
+        return $this->render('contact/index.html.twig', [
+            'contacts' => $contactRepository->search($search),
+            'search' => $search,
+        ]);
     }
 
     #[Route('/contact/{id}', name: 'app_contact_show', requirements: ['id' => '\d+'])]
     public function show(Contact $contact): Response
     {
-        return $this->render('contact/show.html.twig', ['contact' => $contact]);
+        return $this->render('contact/show.html.twig', [
+            'contact' => $contact,
+        ]);
     }
 }

f2ee28ca3ebb05eceefaa23e8bde1985b045f78e
Allan Laheu <allan.laheu@etudiant.univ-reims.fr>
2024-11-27 10:37:34 +0100

    CategoryFixtures: modification du générateur permettant l'affectation d'une catégorie dans 90% des cas

--- src/DataFixtures/ContactFixtures.php
+++ src/DataFixtures/ContactFixtures.php
@@ -2,14 +2,24 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\CategoryFactory;
 use App\Factory\ContactFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+
+use function Zenstruck\Foundry\faker;
 
 class ContactFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        ContactFactory::createMany(150);
+        ContactFactory::createMany(150, function () {
+            $category = ['category' => null];
+            if (faker()->boolean(90)) {
+                $category = ['category' => CategoryFactory::random()];
+            }
+
+            return $category;
+        });
     }
 }

59b9c6fd87b2b7be5698bbea7adbbc01be4113ca
Allan Laheu <allan.laheu@etudiant.univ-reims.fr>
2024-12-04 12:09:36 +0100

    ajout MapEntity au paramètre Contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Repository\ContactRepository;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
@@ -21,7 +22,7 @@ class ContactController extends AbstractController
     }
 
     #[Route('/contact/{id}', name: 'app_contact_show', requirements: ['id' => '\d+'])]
-    public function show(Contact $contact): Response
+    public function show(#[MapEntity(expr: 'repository.findWithCategory(id)')] Contact $contact): Response
     {
         return $this->render('contact/show.html.twig', [
             'contact' => $contact,

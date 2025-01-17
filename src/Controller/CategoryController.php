93bf68daa832689f5ac9aa200a0e730bccaa4cdb
Allan <allan.laheu@etudiant.univ-reims.fr>
2025-01-17 01:01:23 +0100

    Autorisation accès à l'ensemble du controleur uniquement aux utilisateurs connectés

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -7,7 +7,9 @@ use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('IS_AUTHENTICATED_FULLY')]
 class CategoryController extends AbstractController
 {
     #[Route('/category', name: 'app_category')]

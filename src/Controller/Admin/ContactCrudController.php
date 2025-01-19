8aceddc2d3e52c1f74932805a262af68cb979ee5
Allan <allan.laheu@etudiant.univ-reims.fr>
2025-01-19 17:46:30 +0100

    Configuration des champs pour l'entité Contact

--- src/Controller/Admin/ContactCrudController.php
+++ src/Controller/Admin/ContactCrudController.php
@@ -4,8 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Contact;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TelephoneField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ContactCrudController extends AbstractCrudController
@@ -15,14 +16,14 @@ class ContactCrudController extends AbstractCrudController
         return Contact::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('firstname'),
+            TextField::new('lastname'),
+            EmailField::new('email'),
+            TelephoneField::new('phone'),
         ];
     }
-    */
 }

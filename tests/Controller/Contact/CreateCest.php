2eba48bf7f849a4fe3504f75a420a2f3b7de4a8e
Allan <allan.laheu@etudiant.univ-reims.fr>
2025-01-19 17:19:06 +0100

    Correction test pour que seuls les utilisateurs connectés puissent créer un contact

--- tests/Controller/Contact/CreateCest.php
+++ tests/Controller/Contact/CreateCest.php
@@ -2,14 +2,21 @@
 
 namespace App\Tests\Controller\Contact;
 
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
 
 class CreateCest
 {
     public function form(ControllerTester $I): void
     {
-        $I->amOnPage('/contact/create');
+        $user = UserFactory::createOne(
+            [
+                'roles' => ['ROLE_USER'],
+            ]
+        )->_real();
 
+        $I->amLoggedInAs($user);
+        $I->amOnPage('/contact/create');
         $I->seeInTitle("Création d'un nouveau contact");
         $I->see("Création d'un nouveau contact", 'h1');
     }

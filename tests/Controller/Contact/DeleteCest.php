83c60dccfcdc996c8fc8031753b5f5be45418bad
Allan <allan.laheu@etudiant.univ-reims.fr>
2025-01-19 17:15:58 +0100

    Correction test

--- tests/Controller/Contact/DeleteCest.php
+++ tests/Controller/Contact/DeleteCest.php
@@ -3,12 +3,20 @@
 namespace App\Tests\Controller\Contact;
 
 use App\Factory\ContactFactory;
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
 
 class DeleteCest
 {
     public function form(ControllerTester $I): void
     {
+        $user = UserFactory::createOne(
+            [
+                'roles' => ['ROLE_ADMIN'],
+            ]
+        )->_real();
+
+        $I->amLoggedInAs($user);
         ContactFactory::createOne([
             'firstname' => 'Homer',
             'lastname' => 'Simpson',

04cd18099215e16be8402f63b1e1c99e58ef090d
Allan <allan.laheu@etudiant.univ-reims.fr>
2025-01-19 19:14:30 +0100

    Ajout des champs pour l'entité User

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,9 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -15,14 +17,25 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('firstname'),
+            TextField::new('lastname'),
+            EmailField::new('email'),
+            TextField::new('password')
+            ->onlyOnForms()
+            ->setFormType(PasswordType::class)
+            ->setFormTypeOptions(
+                [
+                    'required' => false,
+                    'mapped' => false,
+                    'empty_data' => '',
+                    'attr' => ['autocomplete' => 'new-password'],
+                ]
+            ),
+            ArrayField::new('roles'),
         ];
     }
-    */
 }

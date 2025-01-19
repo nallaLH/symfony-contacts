cef306bb52819fbb3b8970d3f1bec6b9e86bce5a
Allan <allan.laheu@etudiant.univ-reims.fr>
2025-01-19 17:17:19 +0100

    Ajout test vérifiant que seuls les admins ont accès aux ressources permettant de supprimer un contact

--- tests/Controller/Contact/DeleteCest.php
+++ tests/Controller/Contact/DeleteCest.php
@@ -35,4 +35,18 @@ class DeleteCest
         $I->amOnPage("/contact/{$contactId}/delete");
         $I->seeCurrentRouteIs('app_login');
     }
+
+    public function accessIsRestrictedToAdminUsers(ControllerTester $I): void
+    {
+        $contact = ContactFactory::createOne();
+        $user = UserFactory::createOne(
+            [
+                'roles' => ['ROLE_USER'],
+            ]
+        )->_real();
+        $I->amLoggedInAs($user);
+        $contactId = $contact->getId();
+        $I->amOnPage("/contact/{$contactId}/delete");
+        $I->seeResponseCodeIs(403);
+    }
 }
